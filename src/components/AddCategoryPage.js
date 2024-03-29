94fd186d012cbd083251e1f0af75a688fcb0cdbc
Sanjit Mann <sumatmann@gmail.com>
2024-03-28 22:19:28 -0700

    test

--- src/components/AddCategoryPage.js
+++ src/components/AddCategoryPage.js
@@ -18,6 +18,7 @@ function AddCategoryPage ({closeAddCategoryPage}) {
         e.preventDefault();
         try {
             const response = await axios.post('https://ls43udyak5.execute-api.us-east-2.amazonaws.com/categories', formData);
+            
             console.log(response.data);
             setFormData({
                 categoryName: ''
